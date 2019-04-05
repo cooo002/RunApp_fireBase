12785fa3540280ec101e2cdadea59285efb484df
cooo002 <cooo002@naver.com>
2019-04-05 16:04:13 +0900

    Initial Commit

---
+++ RunningAppProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RunningAppProject
+//
+//  Created by 김재석 on 05/04/2019.
+//  Copyright © 2019 김재석. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
