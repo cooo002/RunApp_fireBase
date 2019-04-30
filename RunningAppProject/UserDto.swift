b1fb2e969caf0dc0350af3514cfc0c92d2c2cd52
cooo002 <cooo002@naver.com>
2019-05-01 00:25:50 +0900

    ver01

---
+++ RunningAppProject/UserDto.swift
@@ -0,0 +1,13 @@
+//
+//  UserDto.swift
+//  RunningAppProject
+//
+//  Created by 김재석 on 29/04/2019.
+//  Copyright © 2019 김재석. All rights reserved.
+//
+
+import UIKit
+
+class UserDto: NSObject {
+
+}
